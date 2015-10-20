ce917f70141ce56555a6ae779ed3b66bb0bb16d5
James Crockett <jecrockett@gmail.com>
2015-10-20 13:49:46 -0600

    test_can_print_two_nodes passes

--- jungle_beat.rb
+++ jungle_beat.rb
@@ -1,60 +1,26 @@
+require 'pry'
+
 class JungleBeat
-  attr_accessor :head  # => nil
+  attr_accessor :head
 
   def initialize(string)
     @head = Node.new(string)
   end
 
-  def head
-    @head
-  end
-
   def print_list
-    this_node = head
+    this_node = @head
     # hey node, if you're not nothing..what are you?
-    while this_node.data != nil
-      puts "#{this_node.data}"
-      # if i delete the line below, i get an error for undefined method 'data' for NilClass. Why?
-        require 'pry';binding.pry
-
+    list = []
+    while this_node != nil
+      list << this_node.data
       # great, let's go to the next_node
       this_node = this_node.next_node
     end
-
+    list.join(' ')
   end
 
-  # def print_list
-  #   this_node = @head
-  #   list = ""
-  #   # hey node, if you're not nothing..what are you?
-  #   while this_node.data != nil
-  #     # shovel into the output list
-  #     list << this_node.data
-  #     # great, let's go to the next_node
-  #     this_node = this_node.next_node
-  #   end
-  #   puts list
-  # end
-
-  # def print_list
-  #   this_node = @head
-  #   list = ""
-  #   # hey node, if you're not nothing..what are you?
-  #   loop do
-  #     if this_node.data == nil
-  #       break
-  #     else
-  #     # shovel into the output list
-  #     list << this_node.data
-  #     # great, let's go to the next_node
-  #     this_node = this_node.next_node
-  #     end
-  #   end
-  #   list
-  # end
-
   def append(string)
-    this_node = head
+    this_node = @head
     # do you have a link? then it's your problem
     while this_node.next_node != nil
       this_node = this_node.next_node
@@ -66,7 +32,7 @@ class JungleBeat
 end
 
 class Node
-  attr_accessor :data, :next_node  # => nil
+  attr_accessor :data, :next_node
 
   def initialize(data, next_node=nil)
     @data = data
--- jungle_beat_test.rb
+++ jungle_beat_test.rb
@@ -1,49 +1,39 @@
 require 'minitest'              # => true
 require 'minitest/autorun'      # => true
 require_relative 'jungle_beat'  # => true
-require 'pry'                   # => true
+require 'pry'                   # => false
 
 class JungleBeatTest < Minitest::Test
 
   def test_new_node_exists
-    skip
-    jb = JungleBeat.new("hi")  # => #<JungleBeat:0x007f8a15229010 @head=#<Node:0x007f8a15228fe8 @data="hi", @next_node=nil>>
+    jb = JungleBeat.new("hi")  # => #<JungleBeat:0x007ffab397ab48 @head=#<Node:0x007ffab397ab20 @data="hi", @next_node=nil>>
     assert jb                  # => true
   end
 
   def test_can_print_one_node
-    skip
-    jb = JungleBeat.new("hi")         # => #<JungleBeat:0x007f8a152297e0 @head=#<Node:0x007f8a152297b8 @data="hi", @next_node=nil>>
-    assert_equal "hi", jb.print_list
+    jb = JungleBeat.new("hi")         # => #<JungleBeat:0x007ffab39831d0 @head=#<Node:0x007ffab3981d58 @data="hi", @next_node=nil>>
+    assert_equal "hi", jb.print_list  # => true
   end
 
   def test_can_print_two_nodes
-    jb = JungleBeat.new("hi")
-    jb.append("there")
+    jb = JungleBeat.new("hi")               # => #<JungleBeat:0x007ffab397ba20 @head=#<Node:0x007ffab397b9f8 @data="hi", @next_node=nil>>
+    jb.append("there")                      # => #<Node:0x007ffab397b7f0 @data="there", @next_node=nil>
     assert_equal "hi there", jb.print_list
   end
 
 end
 
-# >> Run options: --seed 3596
+# >> Run options: --seed 41027
 # >>
 # >> # Running:
 # >>
-# >> Ehi
-# >> E.
+# >> .F.
 # >>
-# >> Finished in 0.001123s, 2670.7476 runs/s, 890.2492 assertions/s.
+# >> Finished in 0.001105s, 2713.7557 runs/s, 2713.7557 assertions/s.
 # >>
-# >>   1) Error:
-# >> JungleBeatTest#test_can_:
-# >> ArgumentError: wrong number of arguments (0 for 1)
-# >>     /Users/JEC/Turing/1Module/Homework/1020_JungleBeat/jungle_beat_test.rb:18:in `test_can_'
+# >>   1) Failure:
+# >> JungleBeatTest#test_can_print_two_nodes [/Users/JEC/Turing/1Module/Projects/1020_JungleBeat/jungle_beat_test.rb:21]:
+# >> Expected: "hi there"
+# >>   Actual: "there"
 # >>
-# >>
-# >>   2) Error:
-# >> JungleBeatTest#test_can_print_one_node:
-# >> NoMethodError: undefined method `data' for nil:NilClass
-# >>     /Users/JEC/Turing/1Module/Homework/1020_JungleBeat/jungle_beat.rb:11:in `print_list'
-# >>     /Users/JEC/Turing/1Module/Homework/1020_JungleBeat/jungle_beat_test.rb:15:in `test_can_print_one_node'
-# >>
-# >> 3 runs, 1 assertions, 0 failures, 2 errors, 0 skips
+# >> 3 runs, 3 assertions, 1 failures, 0 errors, 0 skips
